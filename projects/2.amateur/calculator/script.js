bd81d98795e0e0d85a62e6f9a9608f25ee413555
qiqicodes <qiqizheng0127@gmail.com>
2020-08-28 00:43:45 -0700

    Simple Calculator Verion 1
    created calculator html + css files
    created js file contains version control checklist

---
+++ projects/2.amateur/calculator/script.js
@@ -0,0 +1,19 @@
+/* 
+Version 1:
+create all buttons(number and operators) and display window
+    1. style.css
+    2. 5 colors using adobe trend color scheme
+
+Version 2:
+    1. show current number or the result of the last operation
+	2. Display "ERR" if > 8 digit limit
+    3. operatorHandler object methods
+	    1. onclick function on the last number entered
+    4. C operator clear the last digit
+    5. AC operator clear all internal work area and set display to 0
+
+Version 3:
+    1. +/- operator change the sign of the number that is currently displayed
+    2. decimal point ('.') button on the entry pad to that allows floating point numbers up to 3 places to be entered and operations to be carried out to the maximum number of decimal places entered for any one number.
+    3. % percentage operator value/100 
+*/
